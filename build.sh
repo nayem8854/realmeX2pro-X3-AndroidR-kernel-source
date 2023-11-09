6653663bf899a7dc9de96ccffb78bcc7e7f5347b
nayem8854 <35515994+nayem8854@users.noreply.github.com>
2023-11-09 10:17:45 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-#Installing necessary components
-! sudo apt-get update
-! sudo apt-get install -y bc git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig libssl-dev ccache python2 cpio
-
-export SOURCE_ROOT=~/workspace/source
-export DEFCONFIG=vendor/realme_sm8150-perf_defconfig
-export MAKE_PATH=/home/gitpod/workspace/source/prebuild/build-tools/linux-x86/bin/
-export CROSS_COMPILE=/home/gitpod/workspace/source/prebuild/bin/aarch64-linux-android-
-export KERNEL_ARCH=arm64
-export KERNEL_DIR=${SOURCE_ROOT}/kernel/msm-4.14
-export KERNEL_OUT=${KERNEL_DIR}/../kernel_out
-export KERNEL_SRC=${KERNEL_OUT}
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export OUT_DIR=${KERNEL_OUT}
-export ARCH=${KERNEL_ARCH}
-export TARGET_INCLUDES=${TARGET_KERNEL_MAKE_CFLAGS}
-export TARGET_LINCLUDES=${TARGET_KERNEL_MAKE_LDFLAGS}
-export TARGET_KERNEL_MAKE_ENV+="CC=~/workspace/source/prebuild/linux-x86/clang-r383902/bin/clang"
-
-make_defconfig()
+function compile() 
 {
-cd ${KERNEL_DIR} && \
-${MAKE_PATH}make -j$(nproc --all) O=${OUT_DIR} ${TARGET_KERNEL_MAKE_ENV} HOSTLDFLAGS="${TARGET_LINCLUDES}" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} ${DEFCONFIG}
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 10G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=github
+export KBUILD_BUILD_USER="nayem8854"
+[ -d "out" ] && rm -rf ../out || mkdir -p ../out
+
+make O=../out ARCH=arm64 RMX1931_defconfig
+
+PATH="${PWD}/../tc/clang-r383902/bin:${PATH}:${PWD}/../gcc/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=../out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/../gcc/los-4.9-64/bin/aarch64-linux-android-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
-compile()
-{
-cd ${OUT_DIR} && \
-${MAKE_PATH}make -j$(nproc --all) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} HOSTCFLAGS="${TARGET_INCLUDES}" HOSTLDFLAGS="${TARGET_LINCLUDES}" O=${OUT_DIR} ${TARGET_KERNEL_MAKE_ENV}
-}
-make_defconfig
 compile
+
+cd ..
+mkdir module
+cp out/drivers/ommc/core/mmc_test.ko module
+cp out/drivers/net/wireless/ath/wil6210/wil6210.ko module
+cp out/drivers/media/usb/gspca/gspca_main.ko module
+cp out/drivers/media/rc/msm-geni-ir.ko module
+cp out/drivers/platform/msm/msm_11ad/msm_11ad_proxy.ko module
+cp out/drivers/media/platform/msm/dvb/adapter/mpq-adapter.ko module
+cp out/drivers/soc/qcom/llcc_perfmon.ko module
+cp out/net/bridge/br_netfilter.ko module
+cp out/drivers/media/platform/msm/dvb/demux/mpq-dmx-hw-plugin.ko module
+cp out/drivers/media/platform/msm/broadcast/tspp.ko module
+cp out/drivers/video/backlight/lcd.ko module
+cp out/drivers/char/rdbg.ko module
+cp out/arch/arm64/boot/Image-dtb module
+cp out/arch/arm64/boot/Image-dtb-hdr module
+cp out/arch/arm64/boot/Image module
+cp out/arch/arm64/boot/dts/19696/sm8150-v2-mtp.dtb module
